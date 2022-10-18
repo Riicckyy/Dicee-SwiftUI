d53ee14d57dfbc0b64dbbcbddb2ca97614ea38ec
Henrique Rossi <marcoshenriquerp@hotmail.com>
2022-10-18 16:49:41 -0300

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Marcos Henrique Rossi Paes on 01/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
